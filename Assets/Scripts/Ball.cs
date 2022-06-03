157b2922de85dddb560db327b667eed2dfac4589
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2022-06-03 16:54:58 -0300

    feat: add moviment to flippers

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector3(0, 0, 50));
+        }
+    }
+}
